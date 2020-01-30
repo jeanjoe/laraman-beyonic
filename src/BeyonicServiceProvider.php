83601e0dfacb0389dd508e05dae3c628f2cf0fd7
Manzede Benard <manzede@gmail.com>
2020-01-31 00:13:30 +0300

    update package

--- src/BeyonicServiceProvider.php
+++ src/BeyonicServiceProvider.php
@@ -7,13 +7,24 @@ use Laraman\Beyonic\Http\Controllers\BeyonicController;
 
 class BeyonicServiceProvider extends ServiceProvider {
 
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
         $this->publishes([
             __DIR__.'/config/beyonic.php' => config_path('beyonic.php'),
+            'config'
         ]);
     }
 
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->mergeConfigFrom(
